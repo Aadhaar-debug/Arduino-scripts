f46dd3794042668eb51cca9d72908c468e1cca93
Aadhaar Koul <75715900+Aadhaar-debug@users.noreply.github.com>
2023-04-01 00:49:19 +0530

    Create Disp;ayTextOnLCD.ino

---
+++ Disp;ayTextOnLCD.ino
@@ -0,0 +1,42 @@
+// the circuit desig for the below code is at https://www.youtube.com/watch?v=wX2qpVEqKW4
+
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Aadhaar koul");
+  delay(1000);
+}
+
+void loop() {
+  
+  // to move it offscreen left:
+  for (int positionCounter = 0; positionCounter < 13; positionCounter++) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    delay(150);
+  }
+
+  // to move it offscreen right:
+  for (int positionCounter = 0; positionCounter < 29; positionCounter++) {
+    // scroll one position right:
+    lcd.scrollDisplayRight();
+    delay(150);
+  }
+
+  // to move it back to center:
+  for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    // wait a bit:
+    delay(150);
+  }
+  // delay at the end of the full loop:
+  delay(1000);
+
+}
